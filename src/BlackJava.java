7bd1d5113198de87efa9d285bc0259aebe66788a
Andrew Davis <andrewsdavis@users.noreply.github.com>
2017-09-30 20:26:12 -0400

    Added greeting message and skeleton for choices

--- src/BlackJava.java
+++ src/BlackJava.java
@@ -1,7 +1,20 @@
 import java.util.*;
 import javax.swing.*;
-public class BlackJava {
-    public static void main(String[] args) {
 
+public class BlackJava {
+    private static final String gameName = "BlackJava";
+    private static final String yourCards = "Here are your cards: ";
+    private static final String dealersCards = "Here are the dealer's cards: ";
+    private static final String yourChoice= "What would you like to do?";
+    public static void main(String[] args) {
+        int choice;
+        String[] options = {"Hit", "Stand"};
+        choice = JOptionPane.showConfirmDialog(null, "Welcome to BlackJava!\nAre you ready to play?", gameName, JOptionPane.YES_NO_OPTION);
+        if (choice != 0) {
+            System.exit(1);
+        }
+
+        choice = JOptionPane.showOptionDialog(null, yourCards + dealersCards + yourChoice, gameName,
+                0, JOptionPane.QUESTION_MESSAGE, null, options, null);
     }
 }
